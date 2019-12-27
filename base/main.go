959e62f13722dfa48fb8a31a80adbf5e4524a2f3
张志伟 <zhangzw050646@yimidida.com>
2019-12-27 15:27:04 +0800

    xxx

--- base/main.go
+++ base/main.go
@@ -1,9 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// --------------- 创建告警组 ------------------------
+type AlarmGroupProfil struct {
+	Name     string   `json:"name"`
+	Owner    string   `json:"owner"`
+	UserList []string `json:"userlist"`
+	ChatID   string   `json:"chatid"`
+}
+
+// 构造map string
+func generateJsonObj(name, owner, chatid string, userlist []string) (alarmProfile string, err error) {
+	alarmProfil := &AlarmGroupProfil{
+		Owner:    owner,
+		Name:     name,
+		UserList: userlist,
+		ChatID:   chatid,
+	}
+	data, err := json.Marshal(alarmProfil)
+	return string(data), err
+}
 
 func main() {
-	ops := "ops-123"
-	data := fmt.Sprintf(" 配置: %v", ops)
-	fmt.Println(data)
+	data, err := generateJsonObj("测试组", "ym050646", "xxxxxxxxxxx", []string{"ym050646", "ym050446"})
+	if err != nil {
+		fmt.Println("error: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("data: ", data)
 }
